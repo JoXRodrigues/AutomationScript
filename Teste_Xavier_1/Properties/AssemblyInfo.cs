f688ae14141e6ba043d22bd5bfacd2d45944b651
jxavi <jxavi@USO>
2023-05-30 15:55:01 -0300

    Adicionar arquivos de projeto.

---
+++ Teste_Xavier_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Teste_Xavier_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Teste_Xavier_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7038EA96-A1E1-41FC-8FAC-157904170DE5")]
+[assembly: AssemblyVersion("1.0.0.0")]
